01bb83dcee7047e8d8e4c1d78100e70fe56f7552
Hicham DIAB <diabhicham987@gmail.com>
2023-12-21 16:18:55 +0100

    setup the css & the main app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import NavBar from './Components/NavBar';
+import "./global.css";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="">
+      <section className="h-screen">
+        <NavBar/>
+      </section>
+      <section className="h-screen">Parallax</section>
+      <section className="h-screen">Services</section>
+      <section className="h-screen">Parallax</section>
+      <section className="h-screen">Portfolio1</section>
+      <section className="h-screen">Portfolio2</section>
+      <section className="h-screen">Portfolio3</section>
+      <section className="h-screen">Contact</section>
+    </div>
+  );
 };
 
 export default App;
