3df7577e4bc1c07d9656f7e7b9f97b224f361156
HicH987 <diabhicham987@gmail.com>
2023-12-23 20:51:47 +0100

    add NavigationLinks and use the click outside close menu hook

--- src/Components/NavBar/SideMenu/SideMenu.jsx
+++ src/Components/NavBar/SideMenu/SideMenu.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useOutsideClick from "./../../../Hooks/useOutsideClick";
 import { MenuToggle } from "./MenuToggle";
+import { NavigationLinks } from "./NavigationLinks";
 
 const variants = {
   open: {
@@ -22,21 +24,29 @@ const variants = {
 };
 
 export default function SideMenu() {
+  const menuRef = useRef();
   const [isOpen, setIsOpen] = useState(false);
+  const closeMenu =  () => isOpen && setIsOpen(false)
+  useOutsideClick(menuRef,closeMenu);
+
+  const links = ["HomePage", "Services", "Portfolio", "Contact"];
 
   return (
     <div className="flex items-center justify-center h-full p-10">
       <motion.div
+        ref={menuRef}
         className="flex flex-col items-center justify-center text-black bg-white"
         animate={isOpen ? "open" : "closed"}
       >
         <motion.div
           className="z-[999] fixed top-0 left-0 bottom-0 w-[300px] bg-white"
           variants={variants}
-        ></motion.div>
+        >
+          <NavigationLinks links={links} closeMenu={closeMenu}></NavigationLinks>
+        </motion.div>
 
         <MenuToggle
-          toggle={() => setIsOpen(!isOpen)}
+          toggle={() => setIsOpen((prev) => !prev)}
           className={
             "z-[999] fixed top-[37px] left-[29px] text-3xl cursor-pointer"
           }
