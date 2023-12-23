7740c600c3ff51c3c9506b721411e52cfacde285
HicH987 <diabhicham987@gmail.com>
2023-12-23 22:39:12 +0100

    Refactor App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import NavBar from './Components/NavBar/NavBar';
-import "./global.css";
+import Cursor from "@/Components/Cursor/Cursor";
+import NavBar from "@/Components/NavBar/NavBar";
+import "@/global.css";
 
 const App = () => {
-  
-  
   return (
-    <div>
-      <section className="h-screen" id='HomePage'>
-        <NavBar/>
+    <main>
+      <Cursor />
+      <section className="h-screen" id="HomePage">
+        <NavBar />
       </section>
-      <section className="h-screen" id='Services'>Parallax</section>
-      <section className="h-screen" >Services</section>
-      <section className="h-screen" >Parallax</section>
-      <section className="h-screen" id='Portfolio'>Portfolio1</section>
-      <section className="h-screen" >Portfolio2</section>
-      <section className="h-screen" >Portfolio3</section>
-      <section className="h-screen" id='Contact'>Contact</section>
-    </div>
+      <section className="h-screen" id="Services">
+        Parallax
+      </section>
+      <section className="h-screen">Services</section>
+      <section className="h-screen">Parallax</section>
+      <section className="h-screen" id="Portfolio">
+        Portfolio1
+      </section>
+      <section className="h-screen">Portfolio2</section>
+      <section className="h-screen">Portfolio3</section>
+      <section className="h-screen" id="Contact">
+        Contact
+      </section>
+    </main>
   );
 };
 
