ece0651b55e9fb321b510fcdf2307b094fd7b7da
HicH987 <diabhicham987@gmail.com>
2023-12-23 22:38:45 +0100

    Add responsive screen sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./src/Components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: { min: "640px" },
+      md: { min: "768px" },
+      lg: { min: "1024px" },
+      xl: { min: "1280px" },
+      "2xl": { min: "1536px" },
+      "max-sd": { max: "639px" },
+      "max-md": { max: "767px" },
+      "max-lg": { max: "1023px" },
+      "max-xl": { max: "1279px" },
+      "max-2xl": { max: "1535px" },
+    },
   },
   plugins: [],
 };
