7283fc55e980fe84f55ed3e7e142103915b89b40
HicH987 <diabhicham987@gmail.com>
2023-12-23 22:42:05 +0100

    use the SocialMediasList to NavBar component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,34 +1,47 @@
 import { motion } from "framer-motion";
 import SideMenu from "./SideMenu/SideMenu";
+import { SocialMediasList } from "./SocialMediasList/SocialMediasList";
+
+const socialMedias = [
+  {
+    name: "facebook",
+    icon: "/facebook.png",
+    url: "https://www.facebook.com/hich987",
+  },
+  {
+    name: "instagram",
+    icon: "/instagram.png",
+    url: "https://www.instagram.com/hich987",
+  },
+  {
+    name: "youtube",
+    icon: "/youtube.png",
+    url: "https://www.youtube.com/channel/UCc9W8o7gqQp9Pb2nQ7v2W2g",
+  },
+  {
+    name: "dribbble",
+    icon: "/dribbble.png",
+    url: "https://dribbble.com/hich987",
+  },
+];
+const links = ["HomePage", "Services", "Portfolio", "Contact"];
 
 export default function NavBar() {
   return (
     <nav className="flex flex-row gap-0 h-[100px] items-center">
-      <SideMenu />
+      <SideMenu links={links} />
+
       <div className="flex flex-row items-center justify-between w-full h-full m-auto max-w-[1366px]">
         <motion.h1
           className="text-lg text-white"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8 }}
+          transition={{ duration: 0.3 }}
         >
           HICH987 DEV
         </motion.h1>
 
-        <ul className="flex flex-row gap-[20px] mr-3">
-          <a href="">
-            <img className="w-5" src="/facebook.png" alt="" />
-          </a>
-          <a href="">
-            <img className="w-5" src="/instagram.png" alt="" />
-          </a>
-          <a href="">
-            <img className="w-5" src="/youtube.png" alt="" />
-          </a>
-          <a href="">
-            <img className="w-5" src="/dribbble.png" alt="" />
-          </a>
-        </ul>
+        <SocialMediasList socialMedias={socialMedias} />
       </div>
     </nav>
   );
