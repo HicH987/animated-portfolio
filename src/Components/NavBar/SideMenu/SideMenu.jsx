212bbccbbaef1226088fee5d6b937c421c4b2fc9
HicH987 <diabhicham987@gmail.com>
2023-12-23 12:29:34 +0100

    Add SideMenu component and MenuToggle component

---
+++ src/Components/NavBar/SideMenu/SideMenu.jsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { MenuToggle } from "./MenuToggle";
+
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(20px at 40px 47px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+export default function SideMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex items-center justify-center h-full p-10">
+      <motion.div
+        className="flex flex-col items-center justify-center text-black bg-white"
+        animate={isOpen ? "open" : "closed"}
+      >
+        <motion.div
+          className="z-[999] fixed top-0 left-0 bottom-0 w-[300px] bg-white"
+          variants={variants}
+        ></motion.div>
+
+        <MenuToggle
+          toggle={() => setIsOpen(!isOpen)}
+          className={
+            "z-[999] fixed top-[37px] left-[29px] text-3xl cursor-pointer"
+          }
+        />
+      </motion.div>
+    </div>
+  );
+}
