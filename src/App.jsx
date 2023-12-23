6c310d7ebb3a79ec9145aab00a7f523ca8d6d686
HicH987 <diabhicham987@gmail.com>
2023-12-23 20:49:37 +0100

    add the id to each section to go to

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import NavBar from './Components/NavBar/NavBar';
 import "./global.css";
 
 const App = () => {
+  
+  
   return (
-    <div className="">
-      <section className="h-screen">
+    <div>
+      <section className="h-screen" id='HomePage'>
         <NavBar/>
       </section>
-      <section className="h-screen">Parallax</section>
-      <section className="h-screen">Services</section>
-      <section className="h-screen">Parallax</section>
-      <section className="h-screen">Portfolio1</section>
-      <section className="h-screen">Portfolio2</section>
-      <section className="h-screen">Portfolio3</section>
-      <section className="h-screen">Contact</section>
+      <section className="h-screen" id='Services'>Parallax</section>
+      <section className="h-screen" >Services</section>
+      <section className="h-screen" >Parallax</section>
+      <section className="h-screen" id='Portfolio'>Portfolio1</section>
+      <section className="h-screen" >Portfolio2</section>
+      <section className="h-screen" >Portfolio3</section>
+      <section className="h-screen" id='Contact'>Contact</section>
     </div>
   );
 };
