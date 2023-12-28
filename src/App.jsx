4ebcdfd4c6e4b0713aea6009ed2e522048457955
Hicham DIAB <diabhicham987@gmail.com>
2023-12-28 14:35:28 +0100

    add the snap-center as tailwind style instead of style in global.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,39 @@ import Cursor from "@/Components/Cursor/Cursor";
 import Hero from "@/Components/Hero/Hero";
 import NavBar from "@/Components/NavBar/NavBar";
 import Parallax from "@/Components/Parallax/Parallax";
+import Portfolio from "@/Components/Portfolio/Portfolio";
 import Services from "@/Components/Services/Services";
 import "@/global.css";
 
 const App = () => {
+  const sectionStyle= "h-screen snap-center"
+  
   return (
     <main className="flex flex-col">
       <Cursor />
 
-      <section className="h-screen" id="HomePage">
+      <section className={`${sectionStyle}`} id="HomePage">
         <NavBar />
         <Hero />
       </section>
 
-      <section className="h-screen" id="Services">
+      <section className={`${sectionStyle}`} id="Services">
         <Parallax type={"services"} />
       </section>
-      <section className="h-screen">
+
+      <section className={`${sectionStyle}`}>
         <Services />
       </section>
-      <section className="h-screen">
+
+      <section className={`${sectionStyle}`}>
         <Parallax />
       </section>
-      <section className="h-screen" id="Portfolio">
-        Portfolio 1
+
+      <section id="Portfolio">
+        <Portfolio />
       </section>
-      <section className="h-screen">Portfolio2</section>
-      <section className="h-screen">Portfolio3</section>
-      <section className="h-screen" id="Contact">
+
+      <section className={`${sectionStyle}`} id="Contact">
         Contact
       </section>
     </main>
