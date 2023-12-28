3f98b95ccf188a94e0340cc351c1f66dcf106753
Hicham DIAB <diabhicham987@gmail.com>
2023-12-28 11:34:48 +0100

    Add in view animation to Services component

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,5 +1,23 @@
 import { motion } from "framer-motion";
 
+const variants = {
+  initial: {
+    opacity: 0,
+    x: -500,
+    y: 0,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 1,
+      type: "tween",
+      staggerChildren: 0.5,
+    },
+  },
+};
+
 export default function Services() {
   const services = [
     {
@@ -28,9 +46,20 @@ export default function Services() {
     color: "#ffa500",
     transition: { duration: 0.3, ease: "easeInOut" },
   };
+
   return (
-    <div className="flex flex-col gap-1 p-5 cursor-default">
-      <div className="flex flex-row items-center justify-end">
+    <motion.div
+      className="flex flex-col space-y-[3%] p-5 cursor-default"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
+      <motion.div
+        className="flex flex-row items-center justify-end"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+      >
         <motion.p
           className="w-1/3 pr-3 text-3xl text-right font-extralight"
           whileHover={textHighlightAnimation}
@@ -38,9 +67,14 @@ export default function Services() {
           I focus on helping your brand grow and move forward
         </motion.p>
         <hr className="w-1/4 border-gray-500" />
-      </div>
+      </motion.div>
 
-      <div className="flex gap-5">
+      <motion.div
+        className="flex gap-5"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+      >
         <motion.img
           className="object-cover h-32 rounded-full w-72 outline outline-[#ffa500]"
           whileHover={{
@@ -56,9 +90,14 @@ export default function Services() {
           </motion.b>{" "}
           Ideas
         </p>
-      </div>
+      </motion.div>
 
-      <div className="flex flex-row-reverse gap-5">
+      <motion.div
+        className="flex flex-row-reverse gap-5"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+      >
         <motion.button
           className="h-32 rounded-full w-72 outline outline-[#ffa500] text-2xl"
           whileHover={{
@@ -76,16 +115,26 @@ export default function Services() {
           </motion.b>{" "}
           Business
         </p>
-      </div>
+      </motion.div>
 
-      <div className="flex flex-col gap-5">
+      <motion.div
+        className="flex flex-col gap-5"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+      >
         <h2 className="text-4xl font-extralight">
           <motion.b className="font-bold" whileHover={textHighlightAnimation}>
             Services
           </motion.b>{" "}
           Offered
         </h2>
-        <ul className="flex ">
+        <motion.ul
+          className="flex "
+          variants={variants}
+          initial="initial"
+          whileInView="animate"
+        >
           {services.map((service, index) => (
             <motion.li
               key={index}
@@ -116,8 +165,8 @@ export default function Services() {
               </motion.button>
             </motion.li>
           ))}
-        </ul>
-      </div>
-    </div>
+        </motion.ul>
+      </motion.div>
+    </motion.div>
   );
 }
