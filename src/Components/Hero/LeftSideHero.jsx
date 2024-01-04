655c579fe0a3e663eeb76a5bbfc382090e0f6f08
Hicham DIAB <diabhicham987@gmail.com>
2024-01-04 16:13:33 +0100

    make LeftSideHero component styles responsive

--- src/Components/Hero/LeftSideHero.jsx
+++ src/Components/Hero/LeftSideHero.jsx
@@ -23,18 +23,28 @@ const itemVariants = {
 };
 
 export function LeftSideHero({ className }) {
+  const responStyles = {
+    container:"max-hero-md:pt-2 max-hero-md:text-center max-hero-md:space-y-2 max-hero-md:relative",
+    text: "max-hero-md:space-y-0 max-hero-md:justify-between max-hero-md:h-[90%]",
+    h1: "max-hero-md:bg-[#161636] max-hero-md:bg-opacity-40 max-hero-md:rounded-md",
+    buttons: "max-hero-md:justify-between max-hero-md:p-5",
+    buttonContact: "max-hero-md:text-white max-hero-md:bg-purple-800",
+    scrollIcon: "max-hero-md:absolute max-hero-md:top-1/3",
+  };
+
   return (
     <motion.div
-      className={`flex flex-col space-y-12 pt-28 ${className}`}
+      className={`flex flex-col space-y-12 pt-28 ${className} ${responStyles.container}`}
       variants={variants}
       initial="closed"
       animate="open"
     >
+      {/* -------Text------- */}
       <motion.div
-        className="flex flex-col space-y-[7vw] font-semibold"
+        className={`flex flex-col space-y-[7vw] font-semibold ${responStyles.text}`}
         variants={itemVariants}
       >
-        <h1 className="flex text-3xl text-slate-300">
+        <h1 className={`flex text-3xl text-slate-300 ${responStyles.h1}`}>
           <motion.span
             initial={{ rotate: 0, scale: 1 }}
             animate={{
@@ -54,7 +64,7 @@ export function LeftSideHero({ className }) {
           <b className="text-purple-800">HICHAM DIAB </b>
         </h1>
         <h1
-          className="text-7xl text-slate-300"
+          className={`text-7xl text-slate-300 ${responStyles.h1}`}
           style={{
             fontSize: " clamp(2.5rem, 1.5rem + 4vw, 4.5rem)",
           }}
@@ -63,7 +73,11 @@ export function LeftSideHero({ className }) {
         </h1>
       </motion.div>
 
-      <motion.div className="flex flex-row space-x-10" variants={itemVariants}>
+      {/* -------Buttons------- */}
+      <motion.div
+        className={`flex flex-row space-x-10  ${responStyles.buttons}`}
+        variants={itemVariants}
+      >
         <motion.button
           className="px-6 py-2 text-white bg-purple-800 rounded-md"
           whileHover={{ scale: 1.2 }}
@@ -72,7 +86,7 @@ export function LeftSideHero({ className }) {
           Hire me
         </motion.button>
         <motion.button
-          className="px-6 py-2 text-purple-800 border border-purple-800 rounded-md"
+          className={`px-6 py-2 text-purple-800 border border-purple-800 rounded-md ${responStyles.buttonContact}`}
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.95 }}
         >
@@ -80,8 +94,9 @@ export function LeftSideHero({ className }) {
         </motion.button>
       </motion.div>
 
+      {/* -------Scroll Down icon------- */}
       <motion.div
-        className="flex items-center justify-center w-14"
+        className={`flex items-center justify-center w-14 ${responStyles.scrollIcon}`}
         variants={itemVariants}
       >
         <motion.img
